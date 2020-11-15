fc761df2917ea3e13c221f0770c4e59c69529fb5
LobnaElHawary <48859264+LobnaElHawary@users.noreply.github.com>
2020-11-15 12:38:53 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int samoora, nobna, intERCOURSE;
+		samoora = 1;
+		nobna = 2;
+		intERCOURSE = samoora + nobna;
 	}
 
 }
