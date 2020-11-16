cc47bf8b49deafcc3b584c9dd9e435ff2f7c7d75
LobnaElHawary <48859264+LobnaElHawary@users.noreply.github.com>
2020-11-16 14:36:34 +0200

    Adding Grid Interfact

---
+++ src/CovidTracker.java
@@ -0,0 +1,35 @@
+//import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+public class CovidTracker {
+	final static int rowCol = 30;
+	
+	public static void main(String[] args) {
+		
+        final JFrame f = new JFrame("Covid Tester");
+        JPanel panel = new JPanel(new GridLayout(rowCol, rowCol,1,1)); //num of rows, cols, spacing pixels 
+        JLabel[][] grid= new JLabel[rowCol][rowCol];
+        
+        for(int i =0;i<rowCol;i++) {
+        	for(int j=0;j<rowCol;j++) {
+        		
+        		 grid[i][j] = new JLabel("-", JLabel.CENTER);
+//        		 if(i == 15 && j ==15)
+//        			 grid[i][j].setBackground(Color.RED);
+        		 grid[i][j].setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+                 panel.add(grid[i][j]);
+        	}
+        }
+
+        f.setContentPane(panel);
+        f.setSize(900, 900);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
